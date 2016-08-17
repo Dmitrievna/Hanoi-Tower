c784d2aca305a2984c4401b63e35dcc9d6f6e67f
girlbananan <buduprogramistom@gmail.com>
2016-08-17 05:25:39 +0400

    Update hanTower.py

--- hanTower.py
+++ hanTower.py
@@ -2,7 +2,6 @@ import copy
 
 def change(lst1,lst2,lst3,Num):
     if ( Num == 1 ):
-    	#print("before:",lst1,lst2,lst3)
     	lst3.insert( lst3.count(0)+1 ,lst1[lst1.count(0)+1] )
     	lst3.remove(0)
      	lst1.remove(lst1[lst1.count(0)+1])
@@ -10,22 +9,14 @@ def change(lst1,lst2,lst3,Num):
      	print(lst1[0]+" - "+ lst3[0])
 
     else:
-    	#print("before:",lst1,lst2,lst3)
-    	#print("1 - 2")
     	change(lst1,lst3,lst2,Num - 1)
-    	#print("result:",lst1,lst2,lst3)
-    	#print("before:",lst1,lst2,lst3)
-    	#print("1 - 3")
         lst3.insert(lst3.count(0)+1,lst1[lst1.count(0)+1])
         lst1.remove(lst1[lst1.count(0)+1])
         lst1.insert(1,0)
         lst3.remove(0)
         print(lst1[0]+" - "+lst3[0])
-        #print("result:",lst1,lst2,lst3)
-        #print("before:",lst1,lst2,lst3)
-        #print("2 - 3")
         change(lst2,lst1,lst3,Num - 1)
-        #print("result:",lst1,lst2,lst3)
+
 
 
 
